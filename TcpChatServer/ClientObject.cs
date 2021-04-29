eef82d5a87320d1b4c4ee3ba50738723bb730075
Dmitriy <dmitry.shebetovsky@gmail.com>
2021-04-30 00:51:14 +0300

    Added TCP Multi threading chat example.

---
+++ TcpChatServer/ClientObject.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpChatServer
+{
+    public class ClientObject
+    {
+        private string userName;
+        private readonly TcpClient client;
+        private readonly ServerObject server;
+
+        protected internal string Id { get; private set; }
+
+        protected internal NetworkStream Stream { get; private set; }
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+        }
+
+        public void Process()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                string message = GetMessage();
+                userName = message;
+
+                message = userName + " entered the chat";
+                server.BroadcastMessage(message, Id);
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    try
+                    {
+                        message = GetMessage();
+                        message = string.Format("{0}: {1}", userName, message);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, Id);
+                    }
+                    catch
+                    {
+                        message = string.Format("{0}: left chat", userName);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, Id);
+
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                server.RemoveConnection(Id);
+                Close();
+            }
+        }
+
+        private string GetMessage()
+        {
+            byte[] data = new byte[64];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+
+            do
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
+        }
+
+        protected internal void Close()
+        {
+            if (Stream != null)
+            {
+                Stream.Close();
+            }
+
+            if (client != null)
+            {
+                client.Close();
+            }
+        }
+    }
+}
