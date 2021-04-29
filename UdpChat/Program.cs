cbc649ac48018e7ac1099e2e55a4a5f98afe1f6a
Dmitriy <dmitry.shebetovsky@gmail.com>
2021-04-30 01:40:20 +0300

    Added UDP with Socket example.

---
+++ UdpChat/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpChat
+{
+    class Program
+    {
+        static int localPort;
+        static int remotePort;
+        static Socket listeningSocket;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the port to receive messages: ");
+            localPort = int.Parse(Console.ReadLine());
+            Console.Write("Enter the port to send messages: ");
+            remotePort = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your message: ");
+            Console.WriteLine();
+
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listen);
+                listeningTask.Start();
+
+                while (true)
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), remotePort);
+                    listeningSocket.SendTo(data, remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private static void Listen()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), localPort);
+                listeningSocket.Bind(localIP);
+
+                while (true)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    Console.WriteLine("{0}:{1} - {2}",
+                        remoteFullIp.Address.ToString(),
+                        remoteFullIp.Port,
+                        builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private static void Close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+        }
+    }
+}
